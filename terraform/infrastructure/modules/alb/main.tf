896520cce220f3a6ea40c6567f17a246d2e1bf3e
cpcwood <cpcwood@outlook.com>
2021-10-11 16:45:50 +0100

    fix alb setup

--- terraform/infrastructure/modules/alb/main.tf
+++ terraform/infrastructure/modules/alb/main.tf
@@ -11,6 +11,14 @@ resource "aws_security_group" "ecs_alb" {
     ipv6_cidr_blocks = ["::/0"]
   }
 
+  egress {
+    protocol = "-1"
+    from_port = 0
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
   tags = {
     Name = "${var.alb_name}-alb"
   }
